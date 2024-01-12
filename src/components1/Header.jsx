1f52341ab62d0d3406bac2255d884ee762708c1d
Ashutosh Kumar <123981820+AshutoshKumar0206@users.noreply.github.com>
2024-01-12 19:55:52 +0530

    Updated Header

--- src/components1/Header.jsx
+++ src/components1/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
 <div className='w-full border shadow-md py-3 fixed top-0 bg-white'>
 <header className='text-center'>
-     <h1 className='text-3xl font-bold uppercase'>Codehelp Blogs</h1> 
+     <h1 className='text-3xl font-bold uppercase'>Blogs-Context</h1> 
     </header>
 </div>
    
